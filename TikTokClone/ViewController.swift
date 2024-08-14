060a03925dd904037e211dadb0816c1452d86888
aptaclino1 <107854593+aptaclino1@users.noreply.github.com>
2024-08-14 13:00:18 +0800

    Initial Commit

---
+++ TikTokClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokClone
+//
+//  Created by Messiah on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TikTokCloneTests/TikTokCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneTests.swift
+//  TikTokCloneTests
+//
+//  Created by Messiah on 8/14/24.
+//
+
+import Testing
+@testable import TikTokClone
+
+struct TikTokCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
